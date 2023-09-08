83989e63fb5421a66fdf20e6dd02802fbb551027
iqpizza6349 <iqpizza6349@gmail.com>
2023-09-08 12:23:39 +0900

    :tada: :: start my post project

---
+++ src/main/java/org/iqpizza/advancedpost/AdvancedPostApplication.java
@@ -0,0 +1,13 @@
+package org.iqpizza.advancedpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedPostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iqpizza/advancedpost/AdvancedPostApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iqpizza.advancedpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
