47b8175b0da4893074ad3024caf86f6bd70b82a1
iqpizza6349 <iqpizza6349@gmail.com>
2023-09-08 17:57:54 +0900

    :construction: :: define post module to using jpa auditing

---
+++ src/main/java/org/iqpizza/advancedpost/post/module/PostModule.java
@@ -0,0 +1,9 @@
+package org.iqpizza.advancedpost.post.module;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class PostModule {
+}
